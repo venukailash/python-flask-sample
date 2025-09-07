2e960fc0e18b40efa154e6b33fc480b14794178c
venukailash <venukailash>
2025-09-07 20:18:42 +0100

    code review

--- src/services/person_service.py
+++ src/services/person_service.py
@@ -5,6 +5,7 @@ class PersonService:
     def get_all():
         return Person.query.all()
     
+    @staticmethod
     def create(person):
         existing_person = Person.query.filter(Person.lname == person.lname).one_or_none()
 
@@ -15,30 +16,31 @@ class PersonService:
         else:
             raise ValueError(f"Person with last name {person.lname} already exists")
         
+    @staticmethod   
     def read_one(lname):
         existing_person = Person.query.filter(Person.lname == lname).one_or_none()
 
-        if existing_person is not None:
-            return existing_person
-        else:
+        if not existing_person:
             raise ValueError(f"Person with last name {lname} not found")
-        
+        return existing_person
+
+    @staticmethod
     def update(lname, person):
         existing_person = Person.query.filter(Person.lname == lname).one_or_none()
         
-        if existing_person is not None:
-            existing_person.fname = person.fname
-            db.session.merge(existing_person)
-            db.session.commit()
-            return existing_person
-        else:
+        if not existing_person:
             raise ValueError(f"Person with last name {lname} not found")
         
+        existing_person.fname = person.fname
+        db.session.commit()
+        return existing_person
+        
+    @staticmethod    
     def remove(lname):
         existing_person = Person.query.filter(Person.lname == lname).one_or_none()
 
-        if existing_person is not None:
-            db.session.delete(existing_person)
-            db.session.commit()
-        else:
+        if not existing_person:
             raise ValueError(f"Person with last name {lname} not found")
+        
+        db.session.delete(existing_person)
+        db.session.commit()
